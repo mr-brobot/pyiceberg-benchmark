2ab53d54fa7470dbb119780859443376406eef53
Josh Wiley <josh@cloudbend.dev>
2023-08-01 13:31:48 +0000

    feat: profiling + benchmarking tools (wip)

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,9 @@
 
 python -m pip install --user --requirement <(poetry export --without-hashes)
 
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.16.1/hyperfine_1.16.1_amd64.deb
+sudo dpkg -i hyperfine_1.16.1_amd64.deb
+rm hyperfine_1.16.1_amd64.deb
+
 SITE_PACKAGES=$(python -m pip show aws-glue-sessions | grep Location | awk '{print $2}') && \
   python -m jupyter kernelspec install --user $SITE_PACKAGES/aws_glue_interactive_sessions_kernel/glue_pyspark
--- scripts/pyiceberg.py
+++ scripts/pyiceberg.py
@@ -1,22 +1,9 @@
-from datetime import datetime, date, time
-from zoneinfo import ZoneInfo
 from pyiceberg.catalog.glue import GlueCatalog
-from pyiceberg.expressions import GreaterThan
 
 catalog = GlueCatalog("cloudbend")
 
 table = catalog.load_table("benchmark.nyc_taxi")
 
-# April 1st, 2023 in NYC timezone
-pickup_datetime = datetime.combine(
-    date(2023, 4, 1), time(0, 0, 0), tzinfo=ZoneInfo("America/New_York")
-)
+result = table.scan(limit=10).to_arrow()
 
-result = (
-    table.scan(limit=10)
-    .filter(GreaterThan("pickup_datetime", pickup_datetime.isoformat()))
-    .to_arrow()
-)
-
-for record in result.to_pylist():
-    print(record)
+assert len(result) == 10
